7e1f847cd01dcc127fd9cc0c2369f09bbdcc2530
Noé Ksiazek <ksianoe@gmail.com>
2017-02-22 16:14:49 +0100

    Luminosité réaliste pour les flares 40mm

---
+++ addons/lights/CfgAmmo.hpp
@@ -0,0 +1,7 @@
+class CfgAmmo {
+    class FlareBase;
+    class F_40mm_White: FlareBase {
+        timeToLive = 43;
+        intensity = 1500000;
+    };
+};
---
+++ addons/lights/script_component.hpp
@@ -0,0 +1,18 @@
+#define COMPONENT lights
+#define COMPONENT_BEAUTIFIED Lights
+#include "\z\rdp\addons\main\script_mod.hpp"
+
+// #define DEBUG_MODE_FULL
+// #define DISABLE_COMPILE_CACHE
+// #define CBA_DEBUG_SYNCHRONOUS
+// #define ENABLE_PERFORMANCE_COUNTERS
+
+#ifdef DEBUG_ENABLED_LIGHTS
+    #define DEBUG_MODE_FULL
+#endif
+
+#ifdef DEBUG_SETTINGS_LIGHTS
+    #define DEBUG_SETTINGS DEBUG_SETTINGS_LIGHTS
+#endif
+
+#include "\z\rdp\addons\main\script_macros.hpp"
