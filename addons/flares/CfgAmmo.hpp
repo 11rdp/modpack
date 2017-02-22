c7cce52b76aba4e908549292475c8c9530ae6947
Noé Ksiazek <ksianoe@gmail.com>
2017-02-22 18:48:48 +0100

    Fix luminosité de la M992 IR

--- addons/flares/CfgAmmo.hpp
+++ addons/flares/CfgAmmo.hpp
@@ -5,6 +5,9 @@ class CfgAmmo {
         typicalSpeed = 76;
         intensity = 1500000;
     };
+    class F_40mm_Cir: F_40mm_White {
+        intensity = 10;
+    };
     class RDP_40mm_FlareCluster: F_40mm_White {
         deflecting = 60;
         timeToLive = 11;
